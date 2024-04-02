be89eb9852ac970bd9a485cdb2328d5a0b8d3fd6
Khue Tran <khuebtran03@gmail.com>
2024-04-02 14:37:32 -0400

    Codes

---
+++ Codes/AllTestsWithReps_Parallel.R
@@ -0,0 +1,392 @@
+## First column contains the values of the observations; 
+## second contains the break-down of factor A (i of them);
+## third contains the break-down of factor B (j of them); 
+
+# The only difference between this file and AllTestsWithReps is 
+# this one does not include the NullDist functions since those will be ran
+# paralleled.
+
+######## APCSSA/APCSSM ########
+## APC CRA ----
+APCCRADts <- function(dataFrame) {
+  ## Number of levels of factors A (I) and B (J), and number of replications per cell (K)
+  I <- nlevels(as.factor(dataFrame[, 2]))
+  J <- nlevels(as.factor(dataFrame[, 3]))
+  K <- nrow(dataFrame)/(I*J)
+  
+  ## Split dataframe into smaller dataframes by factor B (by columns) for aligning
+  dataList <- split(dataFrame, as.factor(dataFrame[, 3]))
+  
+  ## Align by column average
+  for (j in 1:J) { ## looping over the number of levels for factor B (J)
+    dataList[[j]][, 1] <- dataList[[j]][, 1] - mean(dataList[[j]][, 1])
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor A (by rows) for ranking
+  dataList <- split(dataFrame, as.factor(dataFrame[, 2]))
+  
+  ## Rank by row
+  for (i in 1:I) { ## looping over the number of levels for factor A (I)
+    dataList[[i]][, 1] <- rank(dataList[[i]][, 1], ties.method = "average")
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor B (by column) and then by factor A (by row) 
+  ## for calculation
+  dataList <- split(dataFrame, as.factor(dataFrame[, 3]))
+  for (j in 1:J) {
+    dataList[[j]] <- split(dataList[[j]], as.factor(dataList[[j]][, 2]))
+  }
+  
+  ## Calculate all the J*(J-1)/2 values of V_jj'
+  APCCRA <- c()
+  for (j1 in (1:(J - 1))) {
+    for (j2 in ((j1 + 1):J)) {
+      Vjjp <- 0
+      ## Calculate each cross-comparison
+      for (i1 in (1:(I - 1))) {
+        for (i2 in ((i1 + 1):I)) {
+          for (k1 in 1:K) {
+            for (k2 in 1:K) {
+              for (k3 in 1:K) {
+                for (k4 in 1:K) {
+                  Vjjp <- Vjjp + (dataList[[j1]][[i1]][, 1][k1] + dataList[[j2]][[i2]][, 1][k2] - dataList[[j1]][[i2]][, 1][k3] - dataList[[j2]][[i1]][, 1][k4])^2
+                }
+              }
+            }
+          }
+        }
+      }
+      APCCRA <- c(APCCRA, Vjjp)
+    }
+  }
+  APCCRA <- max(APCCRA)
+  
+  ## Scale
+  APCCRAD <- 2*APCCRA/(K^4*I*(I - 1))
+  return(APCCRAD)
+}
+
+## APC RCA ----
+APCRCADts <- function(dataFrame) {
+  ## Number of levels of factors A (I) and B (J), and number of replication per cell (K)
+  I <- nlevels(as.factor(dataFrame[, 2]))
+  J <- nlevels(as.factor(dataFrame[, 3]))
+  K <- nrow(dataFrame)/(I*J) 
+  
+  ## Split dataframe into smaller dataframes by factor A (by rows) for aligning
+  dataList <- split(dataFrame, as.factor(dataFrame[, 2]))
+  
+  ## Aligning by row average
+  for (i in 1:I) { ## looping over the number of levels for factor A (I)
+    dataList[[i]][, 1] <- dataList[[i]][, 1] - mean(dataList[[i]][, 1])
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor B (by columns) for ranking
+  dataList <- split(dataFrame, as.factor(dataFrame[, 3]))
+  
+  ## Rank by column
+  for (j in 1:J) { ## looping over the number of levels for factor B (J)
+    dataList[[j]][, 1] <- rank(dataList[[j]][, 1], ties.method = "average")
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor A (by row) and then by factor B (by column) 
+  ## for calculation
+  dataList <- split(dataFrame, as.factor(dataFrame[, 2]))
+  for (i in 1:I) {
+    dataList[[i]] <- split(dataList[[i]], as.factor(dataList[[i]][, 3]))
+  }
+  
+  ## Calculate all the I*(I-1)/2 values of V_ii'
+  APCRCA <- c()
+  for (i1 in (1:(I - 1))) {
+    for (i2 in ((i1 + 1):I)) {
+      Viip <- 0
+      ## Calculate each cross-comparison
+      for (j1 in (1:(J - 1))) {
+        for (j2 in ((j1 + 1):J)) {
+          for (k1 in 1:K) {
+            for (k2 in 1:K) {
+              for (k3 in 1:K) {
+                for (k4 in 1:K) {
+                  Viip <- Viip + (dataList[[i1]][[j1]][, 1][k1] + dataList[[i2]][[j2]][, 1][k2] - dataList[[i1]][[j2]][, 1][k3] - dataList[[i2]][[j1]][, 1][k4])^2
+                }
+              }
+            }
+          }
+        }
+      }
+      APCRCA <- c(APCRCA, Viip)
+    }
+  }
+  APCRCA <- max(APCRCA)
+  
+  ## Scale
+  APCRCAD <- 2*APCRCA/(K^4*J*(J - 1))
+  return(APCRCAD)
+}
+
+
+## APC CRM ----
+APCCRMDts <- function(dataFrame) {
+  ## Number of levels of factors A (I) and B (J), and number of replication per cell (K)
+  I <- nlevels(as.factor(dataFrame[, 2]))
+  J <- nlevels(as.factor(dataFrame[, 3]))
+  K <- nrow(dataFrame)/(I*J)
+  
+  ## Split dataframe into smaller dataframes by factor B (by columns) for aligning
+  dataList <- split(dataFrame, as.factor(dataFrame[, 3]))
+  
+  ## Align by column median
+  for (j in 1:J) { ## looping over the number of levels for factor B (J)
+    dataList[[j]][, 1] <- dataList[[j]][, 1] - median(dataList[[j]][, 1])
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor A (by rows) for ranking
+  dataList <- split(dataFrame, as.factor(dataFrame[, 2]))
+  
+  ## Rank by row
+  for (i in 1:I) { ## looping over the number of levels for factor A (I)
+    dataList[[i]][, 1] <- rank(dataList[[i]][, 1], ties.method = "average")
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor B (by column) and then by factor A (by row) 
+  ## for calculation
+  dataList <- split(dataFrame, as.factor(dataFrame[, 3]))
+  for (j in 1:J) {
+    dataList[[j]] <- split(dataList[[j]], as.factor(dataList[[j]][, 2]))
+  }
+  
+  ## Calculate all the J*(J-1)/2 values of V_jj'
+  APCCRM <- c()
+  for (j1 in (1:(J - 1))) {
+    for (j2 in ((j1 + 1):J)) {
+      Vjjp <- 0
+      ## Calculate each cross-comparison
+      for (i1 in (1:(I - 1))) {
+        for (i2 in ((i1 + 1):I)) {
+          for (k1 in 1:K) {
+            for (k2 in 1:K) {
+              for (k3 in 1:K) {
+                for (k4 in 1:K) {
+                  Vjjp <- Vjjp + (dataList[[j1]][[i1]][, 1][k1] + dataList[[j2]][[i2]][, 1][k2] - dataList[[j1]][[i2]][, 1][k3] - dataList[[j2]][[i1]][, 1][k4])^2
+                }
+              }
+            }
+          }
+        }
+      }
+      APCCRM <- c(APCCRM, Vjjp)
+    }
+  }
+  APCCRM <- max(APCCRM)
+  ## Scale
+  APCCRMD <- 2*APCCRM/(K^4*I*(I - 1))
+  return(APCCRMD)
+}
+
+
+## APC RCM ----
+APCRCMDts <- function(dataFrame) {
+  ## Number of levels of factors A (I) and B (J), and number of replication per cell (K)
+  I <- nlevels(as.factor(dataFrame[, 2]))
+  J <- nlevels(as.factor(dataFrame[, 3]))
+  K <- nrow(dataFrame)/(I*J)
+  
+  ## Split dataframe into smaller dataframes by factor A (by rows) for aligning
+  dataList <- split(dataFrame, as.factor(dataFrame[, 2]))
+  
+  ## Aligning by row median
+  for (i in 1:I) { ## looping over the number of levels for factor A (I)
+    dataList[[i]][, 1] <- dataList[[i]][, 1] - median(dataList[[i]][, 1])
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor B (by columns) for ranking
+  dataList <- split(dataFrame, as.factor(dataFrame[, 3]))
+  
+  ## Rank by column
+  for (j in 1:J) { ## looping over the number of levels for factor B (J)
+    dataList[[j]][, 1] <- rank(dataList[[j]][, 1], ties.method = "average")
+  }
+  
+  ## Rebind the (new) dataframe by row
+  dataFrame <- do.call("rbind", dataList)
+  
+  ## Split dataframe into smaller dataframes by factor A (by row) and then by factor B (by column) 
+  ## for calculation
+  dataList <- split(dataFrame, as.factor(dataFrame[, 2]))
+  for (i in 1:I) {
+    dataList[[i]] <- split(dataList[[i]], as.factor(dataList[[i]][, 3]))
+  }
+  
+  ## Calculate all the I*(I-1)/2 values of V_ii'
+  APCRCM <- c()
+  for (i1 in (1:(I - 1))) {
+    for (i2 in ((i1 + 1):I)) {
+      Viip <- 0
+      ## Calculate each cross-comparison
+      for (j1 in (1:(J - 1))) {
+        for (j2 in ((j1 + 1):J)) {
+          for (k1 in 1:K) {
+            for (k2 in 1:K) {
+              for (k3 in 1:K) {
+                for (k4 in 1:K) {
+                  Viip <- Viip + (dataList[[i1]][[j1]][, 1][k1] + dataList[[i2]][[j2]][, 1][k2] - dataList[[i1]][[j2]][, 1][k3] - dataList[[i2]][[j1]][, 1][k4])^2
+                }
+              }
+            }
+          }
+        }
+      }
+      APCRCM <- c(APCRCM, Viip)
+    }
+  }
+  APCRCM <- max(APCRCM)
+  ## Scale
+  APCRCMD <- 2*APCRCM/(K^4*J*(J - 1))
+  return(APCRCMD)
+}
+
+## Test statistic for APCSSA ----
+APCSSAts <- function(dataFrame, numTrial) {
+  ## Get the scaled, unstandardized test statistics
+  APCCRAD <- APCCRADts(dataFrame)
+  APCRCAD <- APCRCADts(dataFrame)
+  
+  ## Standardize and pick the max of the two
+  APCCRADstar <- (APCCRAD - APCSSnullDist[1])/APCSSnullDist[2]
+  APCRCADstar <- (APCRCAD - APCSSnullDist[3])/APCSSnullDist[4]
+  
+  APCSSA <- max(APCCRADstar, APCRCADstar)
+  return(APCSSA)
+}
+
+## Test statistic for APCSSM ----
+APCSSMts <- function(dataFrame, numTrial) {
+  ## Get the scaled, unstandardized test statistics
+  APCCRMD <- APCCRMDts(dataFrame)
+  APCRCMD <- APCRCMDts(dataFrame)
+  
+  ## Standardize and pick the max of the two
+  APCCRMDstar <- (APCCRMD - APCSSnullDist[5])/APCSSnullDist[6]
+  APCRCMDstar <- (APCRCMD - APCSSnullDist[7])/APCSSnullDist[8]
+  
+  APCSSM <- max(APCCRMDstar, APCRCMDstar)
+  return(APCSSM)
+}
+
+# APCSSA and APCSSM Test ----
+# Codes to be used in power study, but for this to work, the previous codes need to be run
+APCSSAtest <- function(dataFrame) {
+  APCSSAtestStat <- APCSSAts(dataFrame)
+  if (APCSSAtestStat >= cutOffAPCSSA) {return(1)} else {return(0)}
+}
+
+APCSSMtest <- function(dataFrame) {
+  APCSSMtestStat <- APCSSMts(dataFrame)
+  if (APCSSMtestStat >= cutOffAPCSSM) {return(1)} else {return(0)}
+}
+
+# DeKroon Test ----
+### Test Statistics ----
+DEKRts <- function(dataFrame) {
+  ## Number of levels of factors A (I) and B (J), and number of replication per cell (K)
+  I <- nlevels(as.factor(dataFrame[, 2]))
+  J <- nlevels(as.factor(dataFrame[, 3]))
+  K <- nrow(dataFrame)/(I*J)
+  
+  ## Split dataframe into smaller dataframes by factor B
+  dataList <- split(dataFrame, as.factor(dataFrame[, 3]))
+  
+  grandSumRank <- rep(0, I)
+  SOS <- 0
+  
+  for (j in 1:J) { ## looping over the number of levels for factor B (J)
+    dataList[[j]][, 1] <- rank(dataList[[j]][, 1], ties.method = "average")
+    sumRankWithinBlock <- rep(0, I)
+    
+    for (i in 1:I) {
+      sumRankWithinBlock[i] <- sum(dataList[[j]][which(dataList[[j]][, 2] == i), 1])
+    }
+    
+    SOS <- SOS + sum(sumRankWithinBlock^2)
+    grandSumRank <- grandSumRank + sumRankWithinBlock
+  }
+  
+  TS <- 12*(SOS - sum(grandSumRank^2)/J)/(K^2*I*(K*I + 1))
+  return(TS)
+}
+
+DEKRcutoff <- function(I, J, K, sigLev, numTrial) {
+  cutOff <- rep(0, numTrial)
+  
+  for (i in 1:numTrial) {
+    DEKRnull <- t(matrix(rnorm(I*J*K), nrow = length(I*J*K)))
+    DEKRnull <- cbind(DEKRnull, rep(1:I, each = K, times = J))
+    DEKRnull <- cbind(DEKRnull, rep(1:J, each = I*K))
+    
+    DEKRnull <- as.data.frame(DEKRnull)
+    
+    cutOff[i] <- DEKRts(DEKRnull)
+  }
+  DEKRcutOff <- unname(quantile(cutOff, 1 - sigLev))
+  
+  # Return the cutoff point
+  return(DEKRcutOff)
+}
+
+### Function to find the test statistics given data -----
+DEKRtest <- function(dataFrame) {
+  testStat <- DEKRts(dataFrame)
+  if (testStat >= cutOff_DEKR) {return(1)} else {return(0)}
+}
+
+# F and RT for Interaction ##########
+
+fTest <- function(dataFrame, sigLev) {
+  pVal <- summary(aov(dataFrame[, 1]~as.factor(dataFrame[, 2])*as.factor(dataFrame[, 3])))[[1]][["Pr(>F)"]][3]
+  if(pVal <= sigLev) {return(1)} else {return(0)}
+}
+
+rankTransformTest <- function(dataFrame, sigLev) {
+  pVal <- summary(aov(rank(dataFrame[, 1], ties.method = "average")~as.factor(dataFrame[, 2])*as.factor(dataFrame[, 3])))[[1]][["Pr(>F)"]][3]
+  if(pVal <= sigLev) {return(1)} else {return(0)}
+}
+
+
+# ART, RAOV, and Permutation Test -----
+artTest <- function(dataFrame, sigLev){
+  pVal <- anova(art(unlist(dataFrame[, 1])~as.factor(unlist(dataFrame[, 2]))*as.factor(unlist(dataFrame[, 3]))))[3, 7]
+  if(pVal <= sigLev) {return(1)} else {return(0)}
+}
+
+raovTest <- function(dataFrame, sigLev){
+  pVal <- raov(unlist(dataFrame[,1])~as.factor(unlist(dataFrame[, 2]))*as.factor(unlist(dataFrame[, 3])))[[1]][3,5]
+  if(pVal <= sigLev) {return(1)} else {return(0)}
+}
+
+permTest <- function(dataFrame, sigLev, permReps){
+  pVal <- permuANOVA(unlist(dataFrame[,1]), as.factor(unlist(dataFrame[, 2])), as.factor(unlist(dataFrame[, 3])),  
+                     perm.type="unrestricted", reps= permReps)[3,1]
+  if(pVal <= sigLev) {return(1)} else {return(0)}
+}
---
+++ Codes/Null Dist Parallel Template.R
@@ -0,0 +1,368 @@
+library(tidyverse)
+library(tictoc)
+library(httr)
+library(parallel)
+library(foreach)
+library(doParallel)
+
+url <- "https://alertzy.app/send"
+res <- httr::handle(url)
+config(res, ssl_verifypeer = TRUE)
+key<-"m4gpvv4g0e2dt4p"
+
+## Replace AixBjxK for settings and NumReps for the number of replications per cell ##
+
+# Null Distribution Mean & Variance ----
+## 250k Sim ----
+tic("Null AixBjxK_250k Sim")  
+#### Null Matrices ----
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+# Parallelized foreach loop
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 250000
+APCnull<- NULL
+APCnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCnull[[i]] <- nullData
+}
+# Stop the cluster
+stopCluster(cl)
+
+#### Null Mean & Variance ----
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+nullDistCRA <- unlist(parLapply(cl, APCnull,APCCRADts), use.names = FALSE) 
+nullDistRCA <- unlist(parLapply(cl, APCnull,APCRCADts), use.names = FALSE)
+nullDistCRM <- unlist(parLapply(cl, APCnull,APCCRMDts), use.names = FALSE)
+nullDistRCM <- unlist(parLapply(cl, APCnull,APCRCMDts), use.names = FALSE)
+stopCluster(cl) 
+
+summary<- c(mean(nullDistCRA), sd(nullDistCRA), mean(nullDistRCA), sd(nullDistRCA),
+            mean(nullDistCRM), sd(nullDistCRM), mean(nullDistRCM), sd(nullDistRCM))
+
+## Means and standard deviations of CRA, RCA, CRM, RCM
+APCSSnullDist_AixBjxK<-list(nullDistCRA,nullDistRCA,nullDistCRM,nullDistRCM,summary)
+save(APCSSnullDist_AixBjxK,file="APCSSnullDist_AixBjxK_250kSim.RData")
+
+Null1_250k<-toc()
+Time_Null1_250k<-Null1_250k$toc - Null1_250k$tic
+save(Time_Null1_250k, file="Time Elapsed for Null 1_AixBjxK_250k Sim.RData")
+
+POST(url, body = list(accountKey = key, 
+                      title = "Null Mean & Var for AixBjxK is Done", 
+                      message = "Comp, 250k"), encode = "form", handle = res)
+
+APCSSnullDist_AixBjxK<-NULL
+
+## 100k Sim ----
+tic("Null AixBjxK_100k Sim")  
+#### Null Matrices ----
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+# Parallelized foreach loop
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 100000
+APCnull<- NULL
+APCnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCnull[[i]] <- nullData
+}
+# Stop the cluster
+stopCluster(cl)
+
+#### Null Mean & Variance ----
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+nullDistCRA <- unlist(parLapply(cl, APCnull,APCCRADts), use.names = FALSE) 
+nullDistRCA <- unlist(parLapply(cl, APCnull,APCRCADts), use.names = FALSE)
+nullDistCRM <- unlist(parLapply(cl, APCnull,APCCRMDts), use.names = FALSE)
+nullDistRCM <- unlist(parLapply(cl, APCnull,APCRCMDts), use.names = FALSE)
+stopCluster(cl) 
+
+summary<- c(mean(nullDistCRA), sd(nullDistCRA), mean(nullDistRCA), sd(nullDistRCA),
+            mean(nullDistCRM), sd(nullDistCRM), mean(nullDistRCM), sd(nullDistRCM))
+
+## Means and standard deviations of CRA, RCA, CRM, RCM
+APCSSnullDist_AixBjxK<-list(nullDistCRA,nullDistRCA,nullDistCRM,nullDistRCM,summary)
+save(APCSSnullDist_AixBjxK,file="APCSSnullDist_AixBjxK_100kSim.RData")
+
+Null1_100k<-toc()
+Time_Null1_100k<-Null1_100k$toc - Null1_100k$tic
+save(Time_Null1_100k, file="Time Elapsed for Null 1_AixBjxK_100k Sim.RData")
+
+POST(url, body = list(accountKey = key, 
+                      title = "Null Mean & Var for AixBjxK is Done", 
+                      message = "Comp, 100k"), encode = "form", handle = res)
+
+APCSSnullDist_AixBjxK<-NULL
+
+## 50k Sim ----
+tic("Null AixBjxK_50k Sim")  
+#### Null Matrices ----
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+# Parallelized foreach loop
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 50000
+APCnull<- NULL
+APCnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCnull[[i]] <- nullData
+}
+# Stop the cluster
+stopCluster(cl)
+
+##### Null Mean & Variance ----
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+nullDistCRA <- unlist(parLapply(cl, APCnull,APCCRADts), use.names = FALSE) 
+nullDistRCA <- unlist(parLapply(cl, APCnull,APCRCADts), use.names = FALSE)
+nullDistCRM <- unlist(parLapply(cl, APCnull,APCCRMDts), use.names = FALSE)
+nullDistRCM <- unlist(parLapply(cl, APCnull,APCRCMDts), use.names = FALSE)
+stopCluster(cl) 
+
+summary<- c(mean(nullDistCRA), sd(nullDistCRA), mean(nullDistRCA), sd(nullDistRCA),
+            mean(nullDistCRM), sd(nullDistCRM), mean(nullDistRCM), sd(nullDistRCM))
+
+## Means and standard deviations of CRA, RCA, CRM, RCM
+APCSSnullDist_AixBjxK<-list(nullDistCRA,nullDistRCA,nullDistCRM,nullDistRCM,summary)
+save(APCSSnullDist_AixBjxK,file="APCSSnullDist_AixBjxK_50kSim.RData")
+
+Null1_50k<-toc()
+Time_Null1_50k<-Null1_50k$toc - Null1_50k$tic
+save(Time_Null1_50k, file="Time Elapsed for Null 1_AixBjxK_50k Sim.RData")
+
+POST(url, body = list(accountKey = key, 
+                      title = "Null Mean & Var for AixBjxK is Done", 
+                      message = "Comp, 50k"), encode = "form", handle = res)
+
+APCSSnullDist_AixBjxK<-NULL
+
+# Summary Stats ----
+APCSSNull_results_AixBjxK<-tibble(Stats=c("CRA Mean", "CRA sd","RCA Mean", "RCA sd","CRM Mean","CRM sd","RCM Mean","RCM sd"))%>% 
+  mutate(Value=APCSSnullDist_AixBjxK[[5]]) %>%
+  mutate(SquaredValue=Value^2)
+APCSSNull_results_AixBjxK
+
+# Null Distribution Critical Values for AixBjxK ----
+## 250k Sim ----
+nullDistAPCSSA <-NULL
+nullDistAPCSSM <-NULL
+APCSSnullDist_AixBjxK[[5]]
+APCSSnullDist<- c()
+
+tic("Null Critical Values for AixBjxK for 250k Sim")
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 250000
+APCSSAnull <- NULL
+APCSSMnull <- NULL
+clusterExport(cl,list("APCCRADts","APCRCADts","APCSSAts","APCCRMDts",
+                      "APCRCMDts","APCSSMts","APCSSnullDist"))
+
+# Second null for Average
+APCSSAnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCSSAnull[[i]] <- nullData
+}
+
+nullDistAPCSSA <- unlist(parLapply(cl, APCSSAnull,APCSSAts), use.names = FALSE) 
+save(nullDistAPCSSA,file="APCSSA Null Distribution AixBjxK_250kSim.RData")
+stopCluster(cl)
+
+# Second null for Median
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 250000
+APCSSAnull <- NULL
+APCSSMnull <- NULL
+clusterExport(cl,list("APCCRADts","APCRCADts","APCSSAts","APCCRMDts",
+                      "APCRCMDts","APCSSMts","APCSSnullDist"))
+
+
+APCSSMnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCSSMnull[[i]] <- nullData
+}
+
+nullDistAPCSSM <- unlist(parLapply(cl, APCSSMnull,APCSSMts), use.names = FALSE) 
+save(nullDistAPCSSM,file="APCSSM Null Distribution AixBjxK_250kSim.RData")
+stopCluster(cl)
+
+Null2_250k<-toc()
+Time_Null2_250k<-Null2_250k$toc-Null2_250k$tic
+save(Time_Null2_250k, file="Time elapsed for Critical Values AixBjxK_250kSim.RData")
+
+POST(url, body = list(accountKey = key, 
+                      title = "Critical Values for AixBjxK is Done", 
+                      message = "Comp, 250k Sim"), encode = "form", handle = res)
+
+## 100k Sim ----
+nullDistAPCSSA <-NULL
+nullDistAPCSSM <-NULL
+APCSSnullDist <- NULL
+APCSSnullDist_AixBjxK[[5]]
+APCSSnullDist<- c()
+
+tic("Null Critical Values for AixBjxK for 100k Sim")
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 100000
+APCSSAnull <- NULL
+APCSSMnull <- NULL
+clusterExport(cl,list("APCCRADts","APCRCADts","APCSSAts","APCCRMDts",
+                      "APCRCMDts","APCSSMts","APCSSnullDist"))
+
+# Second null for Average
+APCSSAnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCSSAnull[[i]] <- nullData
+}
+
+nullDistAPCSSA <- unlist(parLapply(cl, APCSSAnull,APCSSAts), use.names = FALSE) 
+save(nullDistAPCSSA,file="APCSSA Null Distribution AixBjxK_100kSim.RData")
+stopCluster(cl)
+
+# Second null for Median
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 100000
+APCSSAnull <- NULL
+APCSSMnull <- NULL
+clusterExport(cl,list("APCCRADts","APCRCADts","APCSSAts","APCCRMDts",
+                      "APCRCMDts","APCSSMts","APCSSnullDist"))
+
+APCSSMnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCSSMnull[[i]] <- nullData
+}
+
+nullDistAPCSSM <- unlist(parLapply(cl, APCSSMnull,APCSSMts), use.names = FALSE) 
+save(nullDistAPCSSM,file="APCSSM Null Distribution AixBjxK_100kSim.RData")
+stopCluster(cl)
+
+Null2_100k<-toc()
+Time_Null2_100k<-Null2_100k$toc-Null2_100k$tic
+save(Time_Null2_100k, file="Time elapsed for Critical Values AixBjxK_100kSim.RData")
+
+POST(url, body = list(accountKey = key, 
+                      title = "Critical Values for AixBjxK is Done", 
+                      message = "Comp, 100k Sim"), encode = "form", handle = res)
+
+## 50k Sim ----
+nullDistAPCSSA <-NULL
+nullDistAPCSSM <-NULL
+APCSSnullDist <- NULL
+APCSSnullDist_AixBjxK[[5]]
+APCSSnullDist<- c()
+
+tic("Null Critical Values for AixBjxK for 50k Sim")
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 50000
+APCSSAnull <- NULL
+APCSSMnull <- NULL
+clusterExport(cl,list("APCCRADts","APCRCADts","APCSSAts","APCCRMDts",
+                      "APCRCMDts","APCSSMts","APCSSnullDist"))
+
+# Second null for Average
+APCSSAnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCSSAnull[[i]] <- nullData
+}
+
+nullDistAPCSSA <- unlist(parLapply(cl, APCSSAnull,APCSSAts), use.names = FALSE) 
+save(nullDistAPCSSA,file="APCSSA Null Distribution AixBjxK_50kSim.RData")
+stopCluster(cl)
+
+# Second null for Median
+cl <- parallel::makeCluster(detectCores()-1)
+registerDoParallel(cl)
+I <- Ai
+J <- Bj
+K <- NumReps
+numTrial <- 50000
+APCSSAnull <- NULL
+APCSSMnull <- NULL
+clusterExport(cl,list("APCCRADts","APCRCADts","APCSSAts","APCCRMDts",
+                      "APCRCMDts","APCSSMts","APCSSnullDist"))
+APCSSMnull <- foreach(i = 1:numTrial) %dopar% {
+  nullData <- data.frame(value = rnorm(I * J * K),
+                         A = rep(1:I, each = K, times = J),
+                         B = rep(1:J, each = I * K)
+  )
+  APCSSMnull[[i]] <- nullData
+}
+
+nullDistAPCSSM <- unlist(parLapply(cl, APCSSMnull,APCSSMts), use.names = FALSE) 
+save(nullDistAPCSSM,file="APCSSM Null Distribution AixBjxK_50kSim.RData")
+stopCluster(cl)
+
+Null2_50k<-toc()
+Time_Null2_50k<-Null2_50k$toc-Null2_50k$tic
+save(Time_Null2_50k, file="Time elapsed for Critical Values AixBjxK_50kSim.RData")
+
+POST(url, body = list(accountKey = key, 
+                      title = "Critical Values for AixBjxK is Done", 
+                      message = "Comp, 50k Sim"), encode = "form", handle = res)
+
+# Critical Values ----
+a <-quantile(nullDistAPCSSA, 0.95)
+sum(nullDistAPCSSA>= a )/length(nullDistAPCSSA)
+a
+b <- quantile(nullDistAPCSSA,0.99)
+sum(nullDistAPCSSA>= b )/length(nullDistAPCSSA)
+b
+c <- quantile(nullDistAPCSSM,0.95)
+sum(nullDistAPCSSM>= c )/length(nullDistAPCSSM)
+c
+d <- quantile(nullDistAPCSSM,0.99)
+sum(nullDistAPCSSM>= d )/length(nullDistAPCSSM)
+d
+
